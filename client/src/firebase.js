10cb9cc9307b7bd29e57277aa53458452c37ab5c
Khush-Shah <khushshah675@gmail.com>
2024-09-02 19:41:14 +0530

    done with oauth after lots of debugging doing it since an 1hr

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  apiKey: "AIzaSyDgXUf_K_vuo-l4pE5R96mYkVv1ZSRSlkk",
+  authDomain: "home-haven-8b5cf.firebaseapp.com",
+  projectId: "home-haven-8b5cf",
+  storageBucket: "home-haven-8b5cf.appspot.com",
+  messagingSenderId: "320167582438",
+  appId: "1:320167582438:web:5aa2b4f85d9fe0d02ab927",
+  measurementId: "G-PYP0V7H4LT"
 };
 
 // Initialize Firebase
